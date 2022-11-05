293ca3c98ab376b90a059cd7c859841abeca10a7
temsisha <40714959+temsisha@users.noreply.github.com>
2022-11-05 15:11:45 +0100

    Update beer.js

--- components/beer.js
+++ components/beer.js
@@ -1,6 +1,6 @@
 export default function Beer ({beer}){
     const {beerName, beerType, beerABV, beerIBU, newBeer, beerLowPrice, beerHighPrice, breweryLogo} = beer && beer.attributes;
-    console.log(beer && beer.attributes.breweryLogo)
+    console.log(beer && beer.attributes.breweryLogo.data)
     return (
         <div className="article-group">
             <div className="logo">
